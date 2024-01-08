824c826b3eed04015ed81cd4778f07f1370e4f55
Tawheed Sarker Aakash <bradwade7@gmail.com>
2024-01-07 17:55:08 -0800

    working on clock

--- README.md
+++ README.md
@@ -1,3 +1,10 @@
 ## Weather App
 
 A simple weather application that has a search bar and shows weather based on the city that has been entered.
+
+# v1.0
+Weather App deplayed
+
+# v1.1
+Bug fixes:
+- added the city not found alert
--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,9 @@
-const apiKey = ""; // get api key from "https://openweathermap.org/
+///////////////////////////////////////////////////////////////////
+// Weather App
+const apiKey = "50e945400c8db430844c7fc3bc1fcd0c"; // get api key from "https://openweathermap.org/
 const searchBtn = document.querySelector("#searchbtn");
 
-class InfoList{
+class WeatherInfoList{
     constructor( doodleVar,descVar,tempVar, cityVar, countryVar, humidityVar, airpressureVar){
     this.doodleVar = doodleVar;
     this.descVar = descVar;
@@ -12,34 +14,50 @@ class InfoList{
     this.airpressureVar = airpressureVar;
     }
     showData() {
-        document.querySelector("#main").setAttribute("src","https://openweathermap.org/img/wn/" + this.doodleVar + "@4x.png");
-        document.querySelector("#main").setAttribute("alt",this.doodleVar);
-        document.querySelector("#description").innerHTML = this.descVar;
-        document.querySelector("#temperature").innerHTML = this.tempVar + "°C";
-        document.querySelector("#locationname").innerHTML = this.cityVar + ", " + this.countryVar;
-        document.querySelector("#humidity h2").innerHTML = this.humidityVar + "%";
-        document.querySelector("#airpressure h2").innerHTML = this.airpressureVar + " kPa";
+        document.querySelector("#weather_main").setAttribute("src","https://openweathermap.org/img/wn/" + this.doodleVar + "@4x.png");
+        document.querySelector("#weather_main").setAttribute("alt",this.doodleVar);
+        document.querySelector("#weather_description").innerHTML = this.descVar;
+        document.querySelector("#weather_temperature").innerHTML = this.tempVar + "°C";
+        document.querySelector("#weather_locationname").innerHTML = this.cityVar + ", " + this.countryVar;
+        document.querySelector("#weather_humidity h2").innerHTML = this.humidityVar + "%";
+        document.querySelector("#weather_airpressure h2").innerHTML = this.airpressureVar + " kPa";
     }
 }
 function afterEventListener(loc){
     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + loc + "&appid=" + apiKey + "&units=metric";
     fetch(apiUrl)
     .then( res => res.json())
-    .then( data => updateInfoList(data));
+    .then( data => updateWeatherInfoList(data));
 }
 
-function updateInfoList(data){
+function updateWeatherInfoList(data){
     if((data.cod).toString() === "404"){
         alert("Error 404! City not Found.");
     }
     else if((data.cod).toString() === "200"){
-        infoList = new InfoList(data.weather[0].icon,data.weather[0].description, (Math.round(data.main.temp)).toString(), data.name, data.sys.country, data.main.humidity.toString(), (data.main.pressure/100).toString());
-    infoList.showData();
+        weatherInfoList = new WeatherInfoList(data.weather[0].icon,data.weather[0].description, (Math.round(data.main.temp)).toString(), data.name, data.sys.country, data.main.humidity.toString(), (data.main.pressure/100).toString());
+        weatherInfoList.showData();
     }
 }
 //main
-var infoList
+var weatherInfoList
 afterEventListener("burnaby");
-searchBtn.addEventListener("click", function(){
-    afterEventListener(document.querySelector("#location").value.trim())
+weather_searchbtn.addEventListener("click", function(){
+    afterEventListener(document.querySelector("#weather_location").value.trim())
 });
+///////////////////////////////////////////////////////////////////
+// Clock App
+
+// function clockEventListener(loc){
+//     var apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=" + loc + "&appid=" + apiKey + "&units=metric";
+//     fetch(apiUrl)
+//     .then( res => res.json())
+//     .then( data => updateWeatherInfoList(data));
+// }
+// //main
+// var infoList
+// clockEventListener("burnaby");
+// clock_searchbtn.addEventListener("click", function(){
+//     clockEventListener(document.querySelector("#weather_location").value.trim())
+// });
+console.log(date.time())
